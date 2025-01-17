48407e7263ee42d177b6d8a1c28558ec45cdf1c7
Don Bosco <don965877@gmail.com>
2025-01-17 07:56:11 +0300

    Application of state

--- alx-project-0x08/pages/index.tsx
+++ alx-project-0x08/pages/index.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
+import ImageCard from "@/components/common/ImageCard";
+import { ImageProps } from "@/interfaces";
+import { useState } from "react";
+
 
 
 const Home: React.FC = () => {
-  /**
-   * Handles the generation of an image based on the user's input.
-   * This function is triggered when the user clicks the "Generate Image" button.
-   */
+  const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>(
+    []
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+
   const handleGenerateImage = async () => {
-    // Log message to indicate the start of image generation
     console.log("Generating Images");
-
-    // Add image generation logic here
-
   };
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl text-gray-400 font-bold mb-2">Image Generation App</h1>
+        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
         <p className="text-lg text-gray-700 mb-4">
           Generate stunning images based on your prompts!
         </p>
@@ -25,6 +28,8 @@ const Home: React.FC = () => {
         <div className="w-full max-w-md">
           <input
             type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
             className="w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
@@ -32,11 +37,17 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
+            {/* {
+              isLoading ? "Loading..." : "Generate Image"
+            } */}
             Generate Image
           </button>
         </div>
+
+        {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl=
+        {imageUrl} prompt={prompt} />}
       </div>
-     </div>
+    </div>
   );
 };
 
